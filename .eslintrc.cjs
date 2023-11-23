b133125e15f62e920fe72feafb6915ffc8915307
derodero24 <k.itsh20524abc@gmail.com>
2023-11-23 14:56:58 +0900

    add eslint-plugin-eslint-comments

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,7 @@ const config = {
     'eslint:recommended',
     'plugin:@typescript-eslint/strict-type-checked',
     'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:@eslint-community/eslint-comments/recommended',
     'plugin:import/recommended',
     'plugin:import/typescript',
     'plugin:unicorn/recommended',
